a8ad2541ae97d38bdec778f3ff1d1026d974bfa7
amansharma <amansha@wharton.upenn.edu>
2019-09-28 13:05:41 -0400

    made a change

--- playground.py
+++ playground.py
@@ -7,6 +7,8 @@ import datetime
 from dateutil.relativedelta import relativedelta
 import yfinance as yf
 import plotly.graph_objects as go
+import requests
+import pandas as pd
 
 app = Dash()
 external_stylesheets = ['https://codepen.io/rmarren1/pen/mLqGRg.css']
@@ -52,36 +54,58 @@ app.layout = html.Div([
 )
 
 def update_page(input_value):
+    grid = dui.Grid(_id="grid", num_rows=12, num_cols=12, grid_padding=0)
+
+    # Stock Chart
     start = datetime.datetime.today() - relativedelta(years=5)
     end = datetime.datetime.today()
 
     try:
         price_data = yf.download(input_value, start, end)
     except:
-        trace_close = go.Scatter(x=list(price_data.index), y = list(price_data.Close), name = "close",
-                                 line = dict(color="#03b1fc"))
-        data = [trace_close]
-        layout = dict(title="Stock Chart for " + input_value, showlegend = False)
-        fig = dict(data=data, layout=layout)
-        graph_test = dcc.Graph(id="Stock Graph", figure=fig)
+        return "Could not get data"
+    trace_close = go.Scatter(x=list(price_data.index), y=list(price_data.Close), name="close",
+                             line=dict(color="#03b1fc"))
+    data = [trace_close]
+    layout = dict(title=input_value + " - Historic Stock Price", showlegend=False)
+    fig = dict(data=data, layout=layout)
+    stock_chart = dcc.Graph(id="Stock Graph", figure=fig)
 
-    grid.add_element(col=2, row=2, width=5, height=5, element=(graph_test))
+    grid.add_element(col=2, row=2, width=6, height=5, element=(stock_chart))
+
+    # Company Description
+    def find_description(list):
+        test = []
+        for item in list:
+            if (len(item) > 100):
+                test.append(item)
+        return test
+    try:
+        page = requests.get('https://finance.yahoo.com/quote/' + input_value + '/profile?p=' + input_value)
+        from lxml import html as html2
+        tree = html2.fromstring(page.content)
+        data = tree.xpath('//p/text()')
+        description = find_description(data)[0]
+        description = str(description)
+        description_output = html.Div([html.H1('Company Description'),
+                                       html.P(description)])
+    except:
+        description_output = html.Div([html.P("Could not find description")])
+
+    grid.add_element(col=8, row=2, width=4, height=6, element=description_output)
 
 
 
 
-    grid = dui.Grid(_id="grid", num_rows=12, num_cols=12, grid_padding=0)
+    data = [['tom', 10], ['nick', 15], ['juli', 14]]
+
+    # Create the pandas DataFrame
+    df = pd.DataFrame(data, columns=['Name', 'Age'])
+
+
 
     grid.add_element(col=4, row=1, width=8, height=1, element=html.Div(
         style={"background-color": "orange", "height": "100%", "width": "100%"}
-    ))
-
-    grid.add_element(col=7, row=2, width=5, height=2, element=html.Div(
-        style={"background-color": "green", "height": "100%", "width": "100%"}
-    ))
-
-    grid.add_element(col=7, row=4, width=5, height=3, element=html.Div(
-        style={"background-color": "red", "height": "100%", "width": "100%"}
     ))
 
     grid.add_element(col=2, row=7, width=5, height=3, element=html.Div(
