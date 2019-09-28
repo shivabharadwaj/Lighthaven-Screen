3c97edab00633ea8f3c71caea252fc2e6ef74053
shivabharadwaj <shiva97231@gmail.com>
2019-09-28 17:32:26 -0400

    latest changes

---
+++ Screening_Logic.py
@@ -0,0 +1,413 @@
+import pandas as pd
+
+
+def execute (ticker):
+    # Helpers to remove %, B, M from tables and convert to float
+    def remove_percent(input):
+        new_input = input
+        if (type(input) == str):
+            x = input.find('%')
+            if (x != -1):
+                new_input = new_input[0:x]
+        return new_input
+
+
+    def remove_bil(input):
+        new_input = input
+        if (type(input) == str):
+            new_input = input
+            x = input.find('B')
+            if (x != -1):
+                new_input = new_input[0:x]
+        return new_input
+
+
+    def remove_mil(input):
+        new_input = input
+        if (type(input) == str):
+            new_input = input
+            x = input.find('M')
+            if (x != -1):
+                new_input = new_input[0:x]
+        return new_input
+
+
+    def remove_dash(input):
+        new_input = input
+        if (type(input) == str):
+            if (input == '-'):
+                new_input = '0'
+        return new_input
+
+
+    def remove_comma(input):
+        new_input = input
+        x = input.find(',')
+        if (x != -1):
+            new_input = new_input.replace(',', '')
+        return new_input
+
+
+    def fix_negative(input):
+        new_input = input
+        x = input.find('(')
+        if (x != -1):
+            new_input = new_input.replace('(', '-')
+        y = input.find(')')
+        if (y != -1):
+            new_input = new_input.replace(')', '')
+        return new_input
+
+
+    def clean_string(input):
+        input = remove_percent(input)
+        input = remove_bil(input)
+        input = remove_mil(input)
+        input = remove_dash(input)
+        input = remove_comma(input)
+        input = fix_negative(input)
+        return (input)
+
+
+    def clean_table(df):
+        for col in range(df.shape[1]):
+            for row in range(df.shape[0]):
+                df.iloc[row][col] = clean_string(df.iloc[row][col])
+                df.iloc[row][col] = float(df.iloc[row][col])
+        return df
+
+
+    def remove_bil_fundamental(input):
+        new_input = input
+
+        if (type(input) == str):
+            x = input.find('B')
+            if (x != -1):
+                new_input = new_input[0:x]
+                new_input = float(new_input) * 1000
+
+            y = input.find('M')
+            if (y != -1):
+                new_input = float(new_input[0:y])
+        return new_input
+
+
+    # Annual Financials
+
+    try:
+        annual_rev_df = pd.read_html('https://www.marketwatch.com/investing/stock/' + ticker + '/financials')[0]
+        annual_NI_df = pd.read_html('https://www.marketwatch.com/investing/stock/' + ticker + '/financials')[1]
+        annual_cash_df = \
+        pd.read_html('https://www.marketwatch.com/investing/stock/' + ticker + '/financials/balance-sheet')[0]
+        annual_debt_df = \
+        pd.read_html('https://www.marketwatch.com/investing/stock/' + ticker + '/financials/balance-sheet')[2]
+        annual_fcf_df = pd.read_html('https://www.marketwatch.com/investing/stock/' + ticker + '/financials/cash-flow')[2]
+    except:
+        print("Could not scrape yearly financial data")
+
+    annual_rev_df.columns.values[0] = "metric"
+    annual_NI_df.columns.values[0] = "metric"
+    annual_cash_df.columns.values[0] = "metric"
+    annual_debt_df.columns.values[0] = "metric"
+    annual_fcf_df.columns.values[0] = "metric"
+
+    annual_rev_df = annual_rev_df.loc[[0, 1], :]  # Sales and Sales Growth
+    annual_EPS_df = annual_NI_df.loc[annual_NI_df.metric == 'EPS (Diluted)']
+    annual_EPS_growth_df = annual_NI_df.loc[annual_NI_df.metric == 'EPS (Diluted) Growth']
+    annual_NI_df = annual_EPS_df.append(annual_EPS_growth_df)  # EPS and EPS Growth
+    annual_cash_df = annual_cash_df.loc[[1, 2], :]  # Cash and ST Securities
+    annual_debt_df = annual_debt_df.loc[annual_debt_df.metric == 'Long-Term Debt']  # Debt
+    annual_fcf_df = annual_fcf_df.loc[annual_fcf_df.metric == 'Free Cash Flow']  # FCF
+
+    annual_financials_df = annual_rev_df.append(
+        annual_NI_df.append(annual_debt_df.append(annual_fcf_df.append(annual_cash_df))))
+    annual_financials_df = annual_financials_df.drop(columns='5-year trend')
+    annual_financials_df = annual_financials_df.set_index(['metric'])
+    annual_financials_df = annual_financials_df.rename({'Sales Growth': 'Sales Growth (%)',
+                                                        'EPS (Diluted) Growth': 'EPS (Diluted) Growth (%)'})
+    annual_financials_df = clean_table(annual_financials_df)
+
+    # Quarterly Financials
+    try:
+        quarterly_financial_rev_df = pd.read_html('https://www.marketwatch.com/investing/stock/' + ticker +
+                                                  '/financials/income/quarter')[0]
+        quarterly_financial_NI_df = pd.read_html('https://www.marketwatch.com/investing/stock/' + ticker +
+                                                 '/financials/income/quarter')[1]
+    except:
+        print("Could not scrape quarterly financial data")
+
+    quarterly_financial_rev_df.columns.values[0] = 'metric'
+    quarterly_financial_NI_df.columns.values[0] = 'metric'
+
+    quarterly_financial_rev_df = quarterly_financial_rev_df.loc[[0, 1], :]  # Sales and Sales Growth
+    quarterly_EPS_df = quarterly_financial_NI_df.loc[quarterly_financial_NI_df.metric == 'EPS (Diluted)']
+    quarterly_EPS_growth_df = quarterly_financial_NI_df.loc[quarterly_financial_NI_df.metric == 'EPS (Diluted) Growth']
+    quarterly_financial_NI_df = quarterly_EPS_df.append(quarterly_EPS_growth_df)  # EPS and EPS Growth
+
+    quarterly_financials_df = quarterly_financial_rev_df.append(quarterly_financial_NI_df)
+    quarterly_financials_df = quarterly_financials_df.drop(columns='5-qtr trend')
+    quarterly_financials_df = quarterly_financials_df.set_index(['metric'])
+    quarterly_financials_df = quarterly_financials_df.rename({'Sales Growth': 'Sales Growth (%)',
+                                                              'EPS (Diluted) Growth': 'EPS (Diluted) Growth (%)'})
+    quarterly_financials_df = clean_table(quarterly_financials_df)
+
+    # Fundamental Data
+    url = 'https://finance.yahoo.com/quote/' + ticker + '?p=' + ticker
+
+    fund_1_df = pd.read_html(url)[0]
+    fund_2_df = pd.read_html(url)[1]
+
+    fundamental_df = pd.concat([fund_1_df, fund_2_df])
+
+    fundamental_df = fundamental_df.reset_index(drop=True)
+    fundamental_df = fundamental_df.iloc[[0, 5, 8, 10], :]
+    fundamental_df.columns = ['metric', 'value']
+    fundamental_df = fundamental_df.set_index(['metric'])
+    fundamental_df.iloc[0][0] = float(fundamental_df.iloc[0][0])
+    fundamental_df.iloc[2][0] = remove_bil_fundamental(fundamental_df.iloc[2][0])
+    fundamental_df.iloc[3][0] = float(fundamental_df.iloc[3][0])
+    fundamental_df = fundamental_df.rename({'Market Cap': 'Market Cap (Mil)'})
+
+
+    # Long Screen Tables
+    # Fast Grower
+    fast_data = [['Revenue Growth >15% last 3 yrs', False], ['EPS Growth >15% last 3 yrs', False],
+                 ['LTD/Cash + Securities <= 2', False], ['Positive FCF last 3 years', False]]
+    fast_grower = pd.DataFrame(fast_data, columns=['FAST GROWER (LONG)', 'Status'])
+    fast_grower = fast_grower.set_index(['FAST GROWER (LONG)'])
+
+    # Stalwart
+    stalwart_data = [['Revenue Growth >10% last 3 yrs', False], ['EPS Growth >10% last 3 yrs', False],
+                     ['Market Cap >$100MM', False], ['Mature company but still growing', 'Subjective']]
+    stalwart = pd.DataFrame(stalwart_data, columns=['STALWART (LONG)', 'Status'])
+    stalwart = stalwart.set_index(['STALWART (LONG)'])
+
+    # Surfer
+    surfer_data = [['Revenue Growth >35% last 3 yrs', False],
+                   ['Company will dominate/create an industry', 'Subjective']]
+    surfer = pd.DataFrame(surfer_data, columns=['SURFER (LONG)', 'Status'])
+    surfer = surfer.set_index(['SURFER (LONG)'])
+
+    # Short Screen Tables
+    # Dead Company
+    dead_data = [['Declining EPS last 4 qts', False], ['Declinging Revenue last 4 qts', False],
+                 ['LTD/Cash + Securities >= 2', False], ['Negative FCF', False]]
+    dead_walking = pd.DataFrame(dead_data, columns=['DEAD COMPANY (SHORT)', 'Status'])
+    dead_walking = dead_walking.set_index(['DEAD COMPANY (SHORT)'])
+
+    # Fad
+    fad_data = [['Revenue Growth >40% last 3 yrs', False], ['Positve Earnings TTM', False],
+                ['EPS declined most recent qtr', False], ['Product/service is a fad', 'Subjective']]
+    fad = pd.DataFrame(fad_data, columns=['FAD STOCK (SHORT)', 'Status'])
+    fad = fad.set_index(['FAD STOCK (SHORT)'])
+
+    # Hot Story
+    hot_data = [['Price increased dramatically, levelling off', 'Subjective'],
+                ['Many competitors', 'Subjective'], ['High PE ratio', False]]
+    hot_story = pd.DataFrame(hot_data, columns=['HOT STORY (SHORT)', 'Status'])
+    hot_story = hot_story.set_index(['HOT STORY (SHORT)'])
+
+
+    # Fast_Grower Screen
+    def fast_rev(df):
+        if (df.iloc[1, -1] >= 14.5 and df.iloc[1, -2] >= 14.5 and df.iloc[1, -3] >= 14.5):
+            return 'True'
+        else:
+            return 'False'
+
+
+    def fast_eps(df):
+        if (df.iloc[3, -1] >= 14.5 and df.iloc[3, -2] >= 14.5 and df.iloc[3, -3] >= 14.5):
+            return 'True'
+        else:
+            return 'False'
+
+
+    def fast_debt_cash(df):
+        cash_securities = df.iloc[6, -1] + df.iloc[7, -1]
+        debt = df.iloc[4, -1]
+        if (debt / cash_securities <= 2):
+            return 'True'
+        else:
+            return 'False'
+
+
+    def fast_fcf(df):
+        if (df.iloc[5, -1] > 0 and df.iloc[5, -2] > 0 and df.iloc[5, -3] > 0):
+            return 'True'
+        else:
+            return 'False'
+
+
+    def update_fast_grower(df):
+        df.iloc[0, 0] = fast_rev(annual_financials_df)
+        df.iloc[1, 0] = fast_eps(annual_financials_df)
+        df.iloc[2, 0] = fast_debt_cash(annual_financials_df)
+        df.iloc[3, 0] = fast_fcf(annual_financials_df)
+        return df
+
+
+    # Stalwart Screen
+    def stalwart_rev(df):
+        if (df.iloc[1, -1] >= 9.5 and df.iloc[1, -2] >= 9.5 and df.iloc[1, -3] >= 9.5):
+            return 'True'
+        else:
+            return 'False'
+
+
+    def stalwart_eps(df):
+        if (df.iloc[3, -1] >= 9.5 and df.iloc[3, -2] >= 9.5 and df.iloc[3, -3] >= 9.5):
+            return 'True'
+        else:
+            return 'False'
+
+
+    def stalwart_market_cap(df):
+        if (df.iloc[2, 0] >= 100):
+            return 'True'
+        else:
+            return 'False'
+
+
+    def update_stalwart(df):
+        df.iloc[0, 0] = stalwart_rev(annual_financials_df)
+        df.iloc[1, 0] = stalwart_eps(annual_financials_df)
+        df.iloc[2, 0] = stalwart_market_cap(fundamental_df)
+        return df
+
+
+    # Surfer Screen
+    def surfer_rev(df):
+        if (df.iloc[1, -1] >= 34.5 and df.iloc[1, -2] >= 34.5 and df.iloc[1, -3] >= 34.5):
+            return 'True'
+        else:
+            return 'False'
+
+
+    def update_surfer(df):
+        df.iloc[0, 0] = surfer_rev(annual_financials_df)
+        return df
+
+
+    # Dead Walking Screen
+    def dead_eps(df):
+        if (df.iloc[3, -1] < 0 and df.iloc[3, -2] < 0 and df.iloc[3, -3] < 0 and df.iloc[3, -4] < 0):
+            return 'True'
+        else:
+            return 'False'
+
+
+    def dead_rev(df):
+        if (df.iloc[1, -1] < 0 and df.iloc[1, -2] < 0 and df.iloc[1, -3] < 0 and df.iloc[1, -4] < 0):
+            return 'True'
+        else:
+            return 'False'
+
+
+    def dead_debt_cash(df):
+        cash_securities = df.iloc[6, -1] + df.iloc[7, -1]
+        debt = df.iloc[4, -1]
+        if (debt / cash_securities >= 2):
+            return 'True'
+        else:
+            return 'False'
+
+
+    def dead_fcf(df):
+        if (df.iloc[5, -1] < 0):
+            return 'True'
+        else:
+            return 'False'
+
+
+    def update_dead(df):
+        df.iloc[0, 0] = dead_eps(quarterly_financials_df)
+        df.iloc[1, 0] = dead_rev(quarterly_financials_df)
+        df.iloc[2, 0] = dead_debt_cash(annual_financials_df)
+        df.iloc[3, 0] = dead_fcf(annual_financials_df)
+        return df
+
+
+    # Fad Screen
+    def fad_rev(df):
+        if (df.iloc[1, -1] >= 39.5 and df.iloc[1, -2] >= 39.5 and df.iloc[1, -3] >= 39.5):
+            return 'True'
+        else:
+            return 'False'
+
+
+    def fad_eps(df):
+        if (df.iloc[2, 1] > 0):
+            return 'True'
+        else:
+            return 'False'
+
+
+    def fad_eps_decline(df):
+        if (df.iloc[3, -1] < 0):
+            return 'True'
+        else:
+            return 'False'
+
+
+    def update_fad(df):
+        df.iloc[0, 0] = fad_rev(annual_financials_df)
+        df.iloc[1, 0] = fad_eps(annual_financials_df)
+        df.iloc[2, 0] = fad_eps_decline(annual_financials_df)
+        return df
+
+
+    # Hot Story Screen
+    def hot_pe(df):
+        if (df.iloc[3, 0] > 30):
+            return 'True'
+        else:
+            return 'False'
+
+
+    def update_hot(df):
+        df.iloc[2, 0] = hot_pe(fundamental_df)
+        return df
+
+
+    # Display All Screens
+    final_output = []
+
+    s1 = update_fast_grower(fast_grower)
+    s1 = s1.reset_index()
+
+    s2 = update_stalwart(stalwart)
+    s2 = s2.reset_index()
+
+    s3 = update_surfer(surfer)
+    s3 = s3.reset_index()
+
+    s4 = update_dead(dead_walking)
+    s4 = s4.reset_index()
+
+    s5 = update_fad(fad)
+    s5 = s5.reset_index()
+
+    s6 = update_hot(hot_story)
+    s6 = s6.reset_index()
+
+
+    final_output.append(s1)
+    final_output.append(s2)
+    final_output.append(s3)
+    final_output.append(s4)
+    final_output.append(s5)
+    final_output.append(s6)
+    final_output.append(fundamental_df)
+    final_output.append(annual_financials_df)
+    final_output.append(quarterly_financials_df)
+
+    return final_output
+
+
+
+
+
+
+
+
--- playground.py
+++ playground.py
@@ -1,73 +1,74 @@
+import dash_table
 from dash import Dash
 import dash_html_components as html
 import dash_core_components as dcc
 import dash_ui as dui
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import datetime
 from dateutil.relativedelta import relativedelta
 import yfinance as yf
 import plotly.graph_objects as go
 import requests
 import pandas as pd
+from Screening_Logic import execute
 
 app = Dash()
 external_stylesheets = ['https://codepen.io/rmarren1/pen/mLqGRg.css']
 app = Dash(__name__, external_stylesheets=external_stylesheets, )
 
 colors = {
-    'background': '#111111',
+    'background': '#050505',
     'text': '#7FDBFF'
 }
 
-graph_test = dcc.Graph(
-        id='example-graph-2',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                }
-            }
-        }
-    )
-
 app.layout = html.Div([
     dcc.Input(id = 'ticker', value = 'AMZN', type = 'text'),
+    html.Button('Screen!', id='button'),
     html.Div(id = 'testing'),
-   # dui.Layout(
-   #     grid=grid,
-   # )
-    # style={
-    #     'height': '100vh',
-    #     'width': '100vw'
-    # }
 ])
 
-@app.callback(
-    Output(component_id='testing', component_property='children'),
-    [Input(component_id='ticker', component_property='value')]
-)
+# @app.callback(
+#     Output(component_id='testing', component_property='children'),
+#     [Input(component_id='ticker', component_property='value')]
+# )
 
-def update_page(input_value):
+@app.callback(
+    Output('testing', 'children'),
+    [Input('button', 'n_clicks')],
+    [State('ticker', 'value')])
+
+def update_page(n_clicks, ticker):
     grid = dui.Grid(_id="grid", num_rows=12, num_cols=12, grid_padding=0)
+
+    # Black Border
+    grid.add_element(col=1, row=1, width=12, height=1, element=html.Div(
+        style={"background-color": "black", "height": "100%", "width": "100%"}
+    ))
+
+    grid.add_element(col=1, row=12, width=12, height=1, element=html.Div(
+        style={"background-color": "black", "height": "100%", "width": "100%"}
+    ))
+
+    grid.add_element(col=1, row=2, width=1, height=10, element=html.Div(
+        style={"background-color": "black", "height": "100%", "width": "100%"}
+    ))
+
+    grid.add_element(col=12, row=2, width=1, height=10, element=html.Div(
+        style={"background-color": "black", "height": "100%", "width": "100%"}
+    ))
 
     # Stock Chart
     start = datetime.datetime.today() - relativedelta(years=5)
     end = datetime.datetime.today()
 
     try:
-        price_data = yf.download(input_value, start, end)
+        price_data = yf.download(ticker, start, end)
     except:
         return "Could not get data"
     trace_close = go.Scatter(x=list(price_data.index), y=list(price_data.Close), name="close",
                              line=dict(color="#03b1fc"))
     data = [trace_close]
-    layout = dict(title=input_value + " - Historic Stock Price", showlegend=False)
+    layout = dict(title=ticker + " - Historic Stock Price", showlegend=False)
     fig = dict(data=data, layout=layout)
     stock_chart = dcc.Graph(id="Stock Graph", figure=fig)
 
@@ -81,7 +82,7 @@ def update_page(input_value):
                 test.append(item)
         return test
     try:
-        page = requests.get('https://finance.yahoo.com/quote/' + input_value + '/profile?p=' + input_value)
+        page = requests.get('https://finance.yahoo.com/quote/' + ticker + '/profile?p=' + ticker)
         from lxml import html as html2
         tree = html2.fromstring(page.content)
         data = tree.xpath('//p/text()')
@@ -94,46 +95,257 @@ def update_page(input_value):
 
     grid.add_element(col=8, row=2, width=4, height=6, element=description_output)
 
-
-
-
-    data = [['tom', 10], ['nick', 15], ['juli', 14]]
-
-    # Create the pandas DataFrame
-    df = pd.DataFrame(data, columns=['Name', 'Age'])
-
-
-
-    grid.add_element(col=4, row=1, width=8, height=1, element=html.Div(
-        style={"background-color": "orange", "height": "100%", "width": "100%"}
-    ))
-
-    grid.add_element(col=2, row=7, width=5, height=3, element=html.Div(
-        style={"background-color": "green", "height": "100%", "width": "100%"}
-    ))
-
-    grid.add_element(col=7, row=7, width=5, height=2, element=html.Div(
-        style={"background-color": "blue", "height": "100%", "width": "100%"}
-    ))
-
-    grid.add_element(col=2, row=9, width=5, height=2, element=html.Div(
-        style={"background-color": "orange", "height": "100%", "width": "100%"}
-    ))
-
-    grid.add_element(col=7, row=9, width=5, height=2, element=html.Div(
-        style={"background-color": "purple", "height": "100%", "width": "100%"}
-    ))
-
-    grid.add_element(col=2, row=11, width=5, height=2, element=html.Div(
-        style={"background-color": "red", "height": "100%", "width": "100%"}
-    ))
-
-    grid.add_element(col=7, row=11, width=5, height=2, element=html.Div(
+    grid.add_element(col=8, row=7, width=4, height=5, element=html.Div(
         style={"background-color": "yellow", "height": "100%", "width": "100%"}
     ))
 
-    return dui.Layout( grid=grid)
+    fast_df = execute(ticker)[0]
+    fast = dash_table.DataTable(
+        id='table',
+        columns=[{"name": i, "id": i} for i in fast_df.columns],
+        data=fast_df.to_dict('records'),
+        style_cell = {'textAlign' : 'left'},
+        style_header={
+            'backgroundColor': 'white',
+            'fontWeight': 'bold'
+        },
+        style_data_conditional=[
+            {
+                'if': {'row_index': 'odd'},
+                'backgroundColor': 'rgb(248, 248, 248)'
+            },
+            {
+                'if': {
+                    'column_id': 'Status',
+                    'filter_query': '{Status} eq "True"'
+                },
+                'backgroundColor': '#ACFC6A',
+                'color': 'black',
+            },
+            {
+                'if': {
+                    'column_id': 'Status',
+                    'filter_query': '{Status} eq "False"'
+                },
+                'backgroundColor': '#FF8787',
+                'color': 'black',
+            },
+            {
+                'if': {
+                    'column_id': 'Status'},
+                'width': '25%'
+            }
+        ],
+    )
 
+    grid.add_element(col=2, row=7, width=3, height=3, element=fast)
+
+    stalwart_df = execute(ticker)[1]
+    stalwart = dash_table.DataTable(
+        id='table',
+        columns=[{"name": i, "id": i} for i in stalwart_df.columns],
+        data=stalwart_df.to_dict('records'),
+        style_cell={'textAlign': 'left'},
+        style_header={
+            'backgroundColor': 'white',
+            'fontWeight': 'bold'
+        },
+        style_data_conditional=[
+            {
+                'if': {'row_index': 'odd'},
+                'backgroundColor': 'rgb(248, 248, 248)'
+            },
+            {
+                'if': {
+                    'column_id': 'Status',
+                    'filter_query': '{Status} eq "True"'
+                },
+                'backgroundColor': '#ACFC6A',
+                'color': 'black',
+            },
+            {
+                'if': {
+                    'column_id': 'Status',
+                    'filter_query': '{Status} eq "False"'
+                },
+                'backgroundColor': '#FF8787',
+                'color': 'black',
+            },
+            {
+                'if': {
+                    'column_id': 'Status'},
+                'width': '25%'
+            }
+        ],
+    )
+
+    grid.add_element(col=2, row=9, width=3, height=2, element=stalwart)
+
+    surfer_df = execute(ticker)[2]
+    surfer = dash_table.DataTable(
+        id='table',
+        columns=[{"name": i, "id": i} for i in surfer_df.columns],
+        data=surfer_df.to_dict('records'),
+        style_cell={'textAlign': 'left'},
+        style_header={
+            'backgroundColor': 'white',
+            'fontWeight': 'bold'
+        },
+        style_data_conditional=[
+            {
+                'if': {'row_index': 'odd'},
+                'backgroundColor': 'rgb(248, 248, 248)'
+            },
+            {
+                'if': {
+                    'column_id': 'Status',
+                    'filter_query': '{Status} eq "True"'
+                },
+                'backgroundColor': '#ACFC6A',
+                'color': 'black',
+            },
+            {
+                'if': {
+                    'column_id': 'Status',
+                    'filter_query': '{Status} eq "False"'
+                },
+                'backgroundColor': '#FF8787',
+                'color': 'black',
+            },
+            {
+                'if': {
+                    'column_id': 'Status'},
+                'width': '25%'
+            }
+        ],
+    )
+
+    grid.add_element(col=2, row=11, width=3, height=2, element=surfer)
+
+    dead_df = execute(ticker)[3]
+    dead = dash_table.DataTable(
+        id='table',
+        columns=[{"name": i, "id": i} for i in dead_df.columns],
+        data=dead_df.to_dict('records'),
+        style_cell={'textAlign': 'left'},
+        style_header={
+            'backgroundColor': 'white',
+            'fontWeight': 'bold'
+        },
+        style_data_conditional=[
+            {
+                'if': {'row_index': 'odd'},
+                'backgroundColor': 'rgb(248, 248, 248)'
+            },
+            {
+                'if': {
+                    'column_id': 'Status',
+                    'filter_query': '{Status} eq "True"'
+                },
+                'backgroundColor': '#ACFC6A',
+                'color': 'black',
+            },
+            {
+                'if': {
+                    'column_id': 'Status',
+                    'filter_query': '{Status} eq "False"'
+                },
+                'backgroundColor': '#FF8787',
+                'color': 'black',
+            },
+            {
+                'if': {
+                    'column_id': 'Status'},
+                'width': '25%'
+            }
+        ],
+    )
+
+    grid.add_element(col=5, row=7, width=3, height=2, element=dead)
+
+    fad_df = execute(ticker)[4]
+    fad = dash_table.DataTable(
+        id='table',
+        columns=[{"name": i, "id": i} for i in fad_df.columns],
+        data=fad_df.to_dict('records'),
+        style_cell={'textAlign': 'left'},
+        style_header={
+            'backgroundColor': 'white',
+            'fontWeight': 'bold'
+        },
+        style_data_conditional=[
+            {
+                'if': {'row_index': 'odd'},
+                'backgroundColor': 'rgb(248, 248, 248)'
+            },
+            {
+                'if': {
+                    'column_id': 'Status',
+                    'filter_query': '{Status} eq "True"'
+                },
+                'backgroundColor': '#ACFC6A',
+                'color': 'black',
+            },
+            {
+                'if': {
+                    'column_id': 'Status',
+                    'filter_query': '{Status} eq "False"'
+                },
+                'backgroundColor': '#FF8787',
+                'color': 'black',
+            },
+            {
+                'if': {
+                    'column_id': 'Status'},
+                'width': '25%'
+            }
+        ],
+    )
+
+    grid.add_element(col=5, row=9, width=3, height=2, element=fad)
+
+    hot_df = execute(ticker)[5]
+    hot = dash_table.DataTable(
+        id='table',
+        columns=[{"name": i, "id": i} for i in hot_df.columns],
+        data=hot_df.to_dict('records'),
+        style_cell={'textAlign': 'left'},
+        style_header={
+            'backgroundColor': 'white',
+            'fontWeight': 'bold'
+        },
+        style_data_conditional=[
+            {
+                'if': {'row_index': 'odd'},
+                'backgroundColor': 'rgb(248, 248, 248)'
+            },
+            {
+                'if': {
+                    'column_id': 'Status',
+                    'filter_query': '{Status} eq "True"'
+                },
+                'backgroundColor': '#ACFC6A',
+                'color': 'black',
+            },
+            {
+                'if': {
+                    'column_id': 'Status',
+                    'filter_query': '{Status} eq "False"'
+                },
+                'backgroundColor': '#FF8787',
+                'color': 'black',
+            },
+            {
+                'if': {
+                    'column_id': 'Status'},
+                    'width': '25%'
+            }
+        ],
+    )
+
+    grid.add_element(col=5, row=11, width=3, height=2, element=hot)
+
+    return dui.Layout( grid=grid)
 
 if __name__ == "__main__":
     app.run_server(debug=True)
